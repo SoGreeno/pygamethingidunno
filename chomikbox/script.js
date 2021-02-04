0217dbbf73545d4e44b9ac331570c6247faca88d
Piotr Badełek <piotrekbadelek@gmail.com>
2021-02-04 11:05:27 +0100

    Add files via upload

---
+++ chomikbox/script.js
@@ -0,0 +1,31 @@
+"use strict"; // wytłumaczę tą linijkę kiedy lepiej poznasz JS
+
+let chomikbox = {};
+
+chomikbox.czyMuzykaGra = false;
+// dodajemy do pudełka informację
+// że muzyka w obecnej chwili nie gra
+
+chomikbox.pokazChomika = function() {
+	h(".chomik").css("display", "block"); // pokazujemy pudełko z class="chomik"
+	h("#startuj").css("display", "none"); // chowamy przycisk z id="startuj"
+};
+
+chomikbox.muzyka = function() {
+	if (this.czyMuzykaGra) { // czy muzyka gra?
+		// muzyka gra, zakończmy ją
+		h("audio").getEl().pause();
+		h("img").attr("src", "chomik.png"); /* muzyka
+		już nie gra. Niech chomik zdjemie słuchawki*/
+		this.czyMuzykaGra = false;
+	} else {
+		// jeśli muzyka nie gra, zacznijmy grać
+		h("audio").getEl().play();
+		h("img").attr("src", "chomikMuzyka.jpeg"); // niech chomik założy słuchawki
+		this.czyMuzykaGra = true;
+	};
+	// po rozpoczęciu muzyki mówimy,
+	// że muzyka gra
+	// zmieniamy false na true
+	// this oznacza "to pudełko"
+};
