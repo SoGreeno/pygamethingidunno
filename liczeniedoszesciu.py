eaa015edf6309e4f97ecbcd3e61685976e71a59e
SG4 <59972034+SoGreeno@users.noreply.github.com>
2021-03-07 16:56:20 +0100

    Podstawowe liczenie w pythonie

---
+++ liczeniedoszesciu.py
@@ -0,0 +1,24 @@
+import time
+c = 1
+print(c)
+c += 1
+print(c)
+time.sleep(1)
+c += 1
+print(c)
+time.sleep(1)
+c += 1
+print(c)
+time.sleep(1)
+c += 1
+print(c)
+time.sleep(1)
+c += 1
+print(c)
+time.sleep(1)
+print("jak by co")
+time.sleep(0.5)
+print("to kod jest z książki")
+time.sleep(3)
+print("nie kradziony")
+time.sleep(2)
